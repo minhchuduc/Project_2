05cb2d5d5882f7dce28d965cc8cabd9507be9f8a
Chu Duc Minh <chu.ducminh@gmail.com>
2011-11-02 19:23:44 +0700

    update "Sorted result"

--- Basic_2_3.py
+++ Basic_2_3.py
@@ -1,4 +1,4 @@
-#Coding time: 45min
+#Coding time: 80min
 if __name__ == '__main__':
     file_name = raw_input('File name to process: ')
     input_file = open(file_name, 'r')
@@ -8,7 +8,7 @@ if __name__ == '__main__':
         line = input_file.readline()
         line_num += 1
         if not line: break
-        list_words = line.strip("\n").split(" ")
+        list_words = line.strip("\n").lower().split(" ")
         for word in list_words:
             words_dict.setdefault(word, []).append(line_num)
             """ You can figure the line above by: run Python interpreter, a = {}, help(a)
@@ -16,7 +16,17 @@ if __name__ == '__main__':
 
     freq = {}
     for word in words_dict.keys():
-        freq[word] = len(words_dict[word])
-        words_dict[word] = list(set(words_dict[word]))  #Convert list --> set --> list: muc dich la` de de-duplicate
-        tmp_list = ["%s" % element for element in words_dict[word]] #Convert list like [1,2,3,4] to ['1','2',3','4']
-        print freq[word], word, " ".join(tmp_list)  #Convert list like ['1','2',3','4'] to a string '1 2 3 4'
+        freq[word] = len(words_dict[word]) # Calculate appearance frequency of each word.
+        words_dict[word] = list(set(words_dict[word]))  # Convert list --> set --> list: muc. dich' la de "de-duplicate"
+    
+    import sys
+    def my_sort_criteria(word):
+        max_freq = sys.maxint - freq[word]   
+        return str(max_freq)+word    # Return a string like: '9223372036854775801python', '9223372036854775805is',...
+
+    for word in sorted(words_dict.keys(), key=my_sort_criteria):
+        tmp_list = [ "%s" % element for element in words_dict[word] ]  #Convert list like [1,2,3,4] to ['1','2',3','4']
+        """ Can also use alternative:
+        tmp_list = [ str(element) for element in words_dict[word] ] """
+
+        print freq[word], word, ' '.join(tmp_list)  # Convert list like ['1','2',3','4'] to a string '1 2 3 4'
